b644e8e5aa05e4ee94fb14168b04688c733b5c92
Liangliang Nan <liangliang.nan@gmail.com>
2021-02-05 17:41:49 +0100

    use easy3d::file_system

--- AdTree/main.cpp
+++ AdTree/main.cpp
@@ -28,7 +28,6 @@
 
 
 #include <iostream>
-#include <filesystem>
 
 #include <easy3d/algo/remove_duplication.h>
 #include <easy3d/core/box.h>
@@ -83,10 +82,10 @@ int batch_mode(std::string xyz_file, std::string export_folder){
     prop[0] = cloud->get_model_property<easy3d::dvec3>("translation")[0];
 
     // save branches model
-    fs::path branch_file = fs::path(export_folder) / branch_filename;
+    auto branch_file = export_folder + "/" + branch_filename;
     std::cout << "branch file will be saved at : " << branch_file << std::endl;
 
-    if (!easy3d::SurfaceMeshIO::save(branch_file.string(), mesh)){
+    if (!easy3d::SurfaceMeshIO::save(branch_file, mesh)){
         std::cerr << "Save branch_file failed" << std::endl;
         return EXIT_FAILURE;
     }
